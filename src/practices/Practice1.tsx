efacabf4715b295e1690b0369018857557141c71
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-29 19:31:30 +0900

    Typescript引数型指定練習

---
+++ src/practices/Practice1.tsx
@@ -0,0 +1,17 @@
+export const Practice1 = () => {
+  const calcTotalFee = (num: number) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+
+  const onClickButton = () => {
+    calcTotalFee(1000);
+  };
+
+  return (
+    <div>
+      <p>練習問題：引数の型指定</p>
+      <button onClick={onClickButton}>練習問題１を実行</button>
+    </div>
+  );
+};
